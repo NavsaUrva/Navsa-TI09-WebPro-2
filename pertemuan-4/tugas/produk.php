cc4e125e369afee9d4b23bac90d0ed9d92a4082f
NavsaUrva <nvsaurva@gmail.com~>
2023-03-24 13:57:28 +0700

    praktikum4&5

---
+++ pertemuan-4/tugas/produk.php
@@ -0,0 +1,49 @@
+<?php
+require_once 'header.php';
+require_once 'sidebar.php';
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Halaman Produk</h1>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <!-- Default box -->
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Judul</h3>
+
+              <div class="card-tools">
+              </div>
+            </div>
+            <div class="card-body">
+              Ini adalah produk kami
+            </div>
+            <!-- /.card-footer-->
+          </div>
+          <!-- /.card -->
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php
+require_once 'footer.php';
+?>
